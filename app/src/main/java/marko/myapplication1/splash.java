276f8230f9ee4acaa6f6181142dbe814aac663ea
jagu <jiyavasvani2202@gmail.com>
2017-09-11 15:26:41 +0530

    a

--- app/src/main/java/marko/myapplication1/splash.java
+++ app/src/main/java/marko/myapplication1/splash.java
@@ -1,13 +1,23 @@
 package marko.myapplication1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 public class splash extends AppCompatActivity {
-
+private static int splash_time_out=4000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+                    public void run(){
+                Intent homeIntent = new Intent(splash.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },splash_time_out);
     }
 }
