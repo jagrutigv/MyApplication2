987dc5b8841b2202b9d7a6f5fa602ab49182353b
jagu <jiyavasvani2202@gmail.com>
2017-09-11 16:20:34 +0530

    a

--- app/src/main/java/marko/myapplication1/splash.java
+++ app/src/main/java/marko/myapplication1/splash.java
@@ -2,43 +2,37 @@ package marko.myapplication1;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class splash extends AppCompatActivity {
-private static int splash_time_out=4000;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(new Runnable(){
+        imageView =(ImageView)findViewById(R.id.imageView);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.splash_animation);
+        imageView.setAnimation(animation);
+        animation.setAnimationListener(new Animation.AnimationListener() {
             @Override
-                    public void run(){
-                Animation animation= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.splash_animation);
-                animation.setAnimationListener(new Animation.AnimationListener(){
-                    @Override
-                    public void onAnimationStart(Animation animation) {
-
-                    }
-
-                    @Override
-                    public void onAnimationEnd(Animation animation) {
-                        Intent homeIntent = new Intent(splash.this, MainActivity.class);
-                        startActivity(homeIntent);
-                        finish();
-                    }
-
-                    @Override
-                    public void onAnimationRepeat(Animation animation) {
-
-                    }
-
-
-                });
+            public void onAnimationStart(Animation animation) {
 
             }
-        },splash_time_out);
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                 finish();
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 }
