1fe8c3713863c83dfcb94fdb17db09f9729b3e33
jagu <jiyavasvani2202@gmail.com>
2017-09-11 16:06:36 +0530

    a

--- app/src/main/java/marko/myapplication1/splash.java
+++ app/src/main/java/marko/myapplication1/splash.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class splash extends AppCompatActivity {
 private static int splash_time_out=4000;
@@ -14,9 +16,28 @@ private static int splash_time_out=4000;
         new Handler().postDelayed(new Runnable(){
             @Override
                     public void run(){
-                Intent homeIntent = new Intent(splash.this, MainActivity.class);
-                startActivity(homeIntent);
-                finish();
+                Animation animation= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.splash_animation);
+                animation.setAnimationListener(new Animation.AnimationListener(){
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        Intent homeIntent = new Intent(splash.this, MainActivity.class);
+                        startActivity(homeIntent);
+                        finish();
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+
+
+                });
+
             }
         },splash_time_out);
     }
