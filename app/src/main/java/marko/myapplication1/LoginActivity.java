c8f9eb4d7d17640d88137b266653f3323507e3ec
HIREN4131KINAL <hiren7884@gmail.com>
2017-09-14 13:56:38 +0530

    aaaa

--- app/src/main/java/marko/myapplication1/LoginActivity.java
+++ app/src/main/java/marko/myapplication1/LoginActivity.java
@@ -1,7 +1,10 @@
 package marko.myapplication1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * A login screen that offers login via email/password.
@@ -18,6 +21,15 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
+        Button button =(Button)findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myintent =new Intent(LoginActivity.this,SigninActivity.class);
+                startActivity(myintent);
+            }
+        });
+
 
     }
 
