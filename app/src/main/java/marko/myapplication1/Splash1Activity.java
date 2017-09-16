aecbae79d8bcc780de80509e325be1064302a5b5
jagu <jiyavasvani2202@gmail.com>
2017-09-16 14:59:56 +0530

    a

--- app/src/main/java/marko/myapplication1/Splash1Activity.java
+++ app/src/main/java/marko/myapplication1/Splash1Activity.java
@@ -1,21 +1,22 @@
 package marko.myapplication1;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
+import android.widget.TextView;
 
-public class splash extends AppCompatActivity {
-    ImageView imageView;
+public class Splash1Activity extends Activity {
+
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        imageView =(ImageView)findViewById(R.id.imageView);
+        setContentView(R.layout.activity_splash1);
+        textView =(TextView)findViewById(R.id.textView);
         Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
-        imageView.setAnimation(animation);
+        textView.setAnimation(animation);
         animation.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
@@ -24,7 +25,7 @@ public class splash extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                 finish();
+                finish();
                 startActivity(new Intent(getApplicationContext(),LoginActivity.class));
 
             }
