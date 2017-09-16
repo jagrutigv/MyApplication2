dfe88f6c2f9c8700611b1d9a0c6181253325ccfd
jagu <jiyavasvani2202@gmail.com>
2017-09-16 19:25:57 +0530

    a

--- app/src/main/java/marko/myapplication1/Payment.java
+++ app/src/main/java/marko/myapplication1/Payment.java
@@ -1,13 +1,24 @@
 package marko.myapplication1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class Payment extends AppCompatActivity {
-
+public class Payment extends AppCompatActivity implements View.OnClickListener {
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+        btn=(Button)findViewById(R.id.proceed);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i=new Intent(this,Ticket.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/marko/myapplication1/Ticket.java
@@ -0,0 +1,39 @@
+package marko.myapplication1;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class Ticket extends AppCompatActivity implements View.OnClickListener {
+Button q;
+    AlertDialog alertDialog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket);
+        q=(Button)findViewById(R.id.Quit);
+        q.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        new AlertDialog.Builder(this).setIcon(android.R.drawable.ic_dialog_alert).setTitle("Exit")
+                .setMessage("Are you sure?")
+                .setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        Intent intent = new Intent(Intent.ACTION_MAIN);
+                        intent.addCategory(Intent.CATEGORY_HOME);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                        finish();
+                    }
+                }).setNegativeButton("no", null).show();
+    }
+}
